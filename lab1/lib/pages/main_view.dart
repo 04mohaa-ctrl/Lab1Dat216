907ebe67d24c810d964c723de9d3fadf5a355e6b
04mohaa-ctrl <04mohaa@gmail.com>
2026-03-29 23:16:02 +0200

    Update main_view.dart

--- lab1/lib/pages/main_view.dart
+++ lab1/lib/pages/main_view.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
-
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Things todo'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
